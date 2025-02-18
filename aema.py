03aa04565514563becb1174bb22ab3d444e1d522
HUET Thomas <TH282424@extra.cea.fr>
2025-02-18 13:37:01 +0100

    varia

---
+++ aema.py
@@ -0,0 +1,9 @@
+def gallica_api(api_root = 'https://gallica.bnf.fr/services/OAIRecord?ark=', ark_id= 'btv1b104536783', verbose = True):
+	"""
+	Collect data from APIs
+
+	:param api_root: API root path
+	:param ark_id: Ark 
+	"""
+	
+    pass
