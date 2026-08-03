4d9f650478a611b5c36a00060eebc4c831949ad7
HUET Thomas <TH282424@extra.cea.fr>
2026-08-03 08:55:38 +0200

    renamed interactive editor for python (iep) and archived + _upload_from_gh.sh customized

--- zenodo/_upload_from_gh.sh
+++ zenodo/_upload_from_gh.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+# TODO: modifications needed
+
 # Bulk upload of the dash functions
 # run './_upload_from_gh.sh' to upload the updated functions from GitHub. 
 
 # Define base project directory
-project_dir="/home/ubuntu/dashboard"
+project_dir="/home/ubuntu/zenodo"
 cd "$project_dir" || exit 1
 
 # Create timestamped archive folder inside project
 archive_dir="archive-$(date +%F-%H-%M)"
-gh_root="https://raw.githubusercontent.com/iramat/iramat-functions/refs/heads/main/dashboard"
+gh_root="https://raw.githubusercontent.com/iramat/iramat-functions/refs/heads/main/zenodo"
 
 mkdir "$archive_dir"
 echo "Created folder: $archive_dir"
 
-# Move all contents of dashboard into archive (except self and archive)
+# Move all contents of zenodo into archive (except self and archive)
 rsync -av --remove-source-files \
   --exclude='_upload_from_gh.sh' \
   --exclude="$archive_dir" \
   --exclude='(archives)' \
-  --exclude='assets' \
   ./ "$archive_dir"
 
 echo "All files moved to $archive_dir"
@@ -30,9 +31,5 @@ mv "$archive_dir" "(archives)/"
 echo "All files moved to '(archives)'"
 
 # Re-download fresh files
-wget "$gh_root/app.py"
-wget "$gh_root/urls.py"
-wget "$gh_root/charts.py"
-wget "$gh_root/maps.py"
-wget "$gh_root/get_data.py"
+wget "$gh_root/chips_to_zenodo.py"
 
